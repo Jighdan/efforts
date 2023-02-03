8c94cd7fa2ea0e046be85f735973a46838ea2d37
Reinny Almonte <reinny.almonte.ramos@gmail.com>
2023-02-03 18:23:53 -0400

    style: global styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,66 @@
+const plugin = require("tailwindcss/plugin");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{ts,tsx}'],
+  content: ["./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    colors: {
+      // PRIMARY
+      white: "#FFFCF2",
+      black: "#252422",
+      silver: "#CCC5B9",
+      olive: "#403D39",
+      orange: "#EB5E28",
+
+      // AUXILIARY
+      transparent: "transparent",
+    },
+
+    extend: {
+      container: {
+        center: true,
+        screens: {
+          sm: "100%",
+          md: "360px",
+          lg: "360px",
+          xl: "360px",
+        },
+      },
+    },
   },
-  plugins: [],
-}
+  plugins: [
+    require("tailwindcss-radix")(),
+
+    plugin(function ({ addVariant }) {
+      addVariant("child", "& > *");
+    }),
+
+    plugin(function ({ addUtilities, theme }) {
+      const base = ".size";
+
+      addUtilities({
+        [`${base}-sm`]: {
+          width: theme("width.6"),
+          height: theme("width.6"),
+        },
+        [`${base}-md`]: {
+          width: theme("width.8"),
+          height: theme("width.8"),
+        },
+        [`${base}-lg`]: {
+          width: theme("width.12"),
+          height: theme("width.12"),
+        },
+      });
+    }),
+
+    plugin(function ({ addComponents, theme }) {
+      addComponents({
+        ".layout-main": {
+          display: "grid",
+          gridTemplateRows: "1fr auto",
+        },
+      });
+    }),
+  ],
+};
