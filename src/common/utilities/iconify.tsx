bb38afbe244ab4d0dad657b30cfc3d2f813c512e
Reinny Almonte <reinny.almonte.ramos@gmail.com>
2023-02-03 19:10:20 -0400

    chore: linting utilities

--- src/common/utilities/iconify.tsx
+++ src/common/utilities/iconify.tsx
@@ -1,10 +1,10 @@
-import { cloneElement, HTMLAttributes } from 'react';
-import {Root} from '@radix-ui/react-accessible-icon';
+import { cloneElement, HTMLAttributes } from "react";
+import { Root } from "@radix-ui/react-accessible-icon";
 
-type Props = Pick<HTMLAttributes<HTMLOrSVGElement>, 'className' | 'onClick'>;
+type Props = Pick<HTMLAttributes<HTMLOrSVGElement>, "className" | "onClick">;
 
 export const Icon = (label: string, svgElement: JSX.Element) => {
-  return ({ className = '', onClick }: Props) => {
+  return ({ className = "", onClick }: Props) => {
     const element = cloneElement(svgElement, { className, onClick });
 
     return <Root label={label}>{element}</Root>;
