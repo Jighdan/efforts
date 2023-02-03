35e1b18bde066ecc30cc04a569569ac49f47835a
Reinny Almonte <reinny.almonte.ramos@gmail.com>
2023-02-03 18:25:52 -0400

    feat: defining layout main

---
+++ src/common/layouts/Main/components/Navigation.tsx
@@ -0,0 +1,16 @@
+import { ComponentProps } from "react";
+import { NavigationItem } from "./NavigationItem";
+
+interface Props {
+  items: ComponentProps<typeof NavigationItem>[];
+}
+
+export const Navigation = ({ items }: Props) => {
+  return (
+    <ul className="flex justify-evenly">
+      {items.map((item) => (
+        <NavigationItem key={item.label} {...item} />
+      ))}
+    </ul>
+  );
+};
---
+++ src/common/layouts/Main/index.tsx
@@ -0,0 +1,37 @@
+import { ComponentProps } from "react";
+import { WithChildren } from "~/common/interfaces/with-children";
+import { Navigation } from "./components/Navigation";
+import { Routes } from "~/common/enums/routes";
+import { IconQueueList } from "~/common/assets/icons/IconQueueList";
+import { IconPencil } from "~/common/assets/icons/IconPencil";
+import { IconCalendar } from "~/common/assets/icons/IconCalendar";
+
+type Props = WithChildren;
+
+const navigation: ComponentProps<typeof Navigation>["items"] = [
+  {
+    label: "Efforts",
+    href: Routes.ALL_EFFORTS,
+    icon: <IconQueueList />,
+  },
+  {
+    label: "Home",
+    href: Routes.HOME,
+    icon: <IconPencil />,
+  },
+  {
+    label: "Calendar",
+    href: Routes.CALENDAR,
+    icon: <IconCalendar />,
+  },
+];
+
+export const Layout = ({ children }: Props) => (
+  <div className="min-h-screen max-sm:px-8 md:py-8 container layout-main">
+    {children}
+
+    <footer className="w-full pt-4 border-t-2 border-t-silver hover:border-t-black">
+      <Navigation items={navigation} />
+    </footer>
+  </div>
+);
