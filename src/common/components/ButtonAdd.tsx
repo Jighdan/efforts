fb022b655bfebdfc9a85391b4a94baeae2ffa542
Reinny Almonte <reinny.almonte.ramos@gmail.com>
2023-02-03 19:09:53 -0400

    feat: defining ButtonAdd component

---
+++ src/common/components/ButtonAdd.tsx
@@ -0,0 +1,20 @@
+import { HTMLAttributes } from "react";
+import { IconPlus } from "~/common/assets/icons/IconPlus";
+
+interface Props extends PrimitiveProps {
+  label: string;
+}
+
+type PrimitiveProps = Pick<HTMLAttributes<HTMLButtonElement>, "onClick">;
+
+export const ButtonAdd = ({ label, onClick }: Props) => (
+  <button
+    title={label}
+    onClick={onClick}
+    className="group focus:outline-none"
+    type="button"
+    aria-label={label}
+  >
+    <IconPlus className="size-md stroke-silver group-hover:stroke-black group-focus:stroke-black" />
+  </button>
+);
