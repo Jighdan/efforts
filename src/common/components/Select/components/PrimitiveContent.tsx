f08f09073df0781aa748a6bd07403eaa534d9e69
Reinny Almonte <reinny.almonte.ramos@gmail.com>
2023-02-04 22:07:18 -0400

    refactor: forwarding refs + splitting Select component

---
+++ src/common/components/Select/components/PrimitiveContent.tsx
@@ -0,0 +1,30 @@
+import { forwardRef } from "react";
+import { Content, Viewport, Item, ItemText } from "@radix-ui/react-select";
+import { PropsWithoutControl } from "../interfaces";
+
+type Props = Pick<PropsWithoutControl, "items">;
+
+export const PrimitiveContent = forwardRef<HTMLDivElement, Props>(
+  ({ items }, ref) => (
+    <Content
+      position="popper"
+      side="bottom"
+      align="start"
+      className="w-[var(--radix-select-trigger-width)]"
+      ref={ref}
+    >
+      <Viewport className="bg-white">
+        {items.map((item) => (
+          <Item
+            key={item.value}
+            value={item.value}
+            disabled={item.disabled}
+            className="p-2 focus:outline-none select-none hover:bg-silver"
+          >
+            <ItemText>{item.label}</ItemText>
+          </Item>
+        ))}
+      </Viewport>
+    </Content>
+  )
+);
