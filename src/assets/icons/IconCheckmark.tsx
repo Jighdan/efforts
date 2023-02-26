6ab8e36ff90d4de440ba71c189dd2fbc95d73955
Reinny Almonte <reinny.almonte.ramos@gmail.com>
2023-02-25 23:13:29 -0400

    feat: adding icon checkmark asset

---
+++ src/assets/icons/IconCheckmark.tsx
@@ -0,0 +1,18 @@
+import { Icon } from "~/utilities/iconify";
+
+export const IconCheckmark = Icon(
+  "icon-checkmark",
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M4.5 12.75l6 6 9-13.5"
+    />
+  </svg>
+);
