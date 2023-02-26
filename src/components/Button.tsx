34c1008fbce7c63016853a4002dc5b1a9dd3ec7b
Reinny Almonte <reinny.almonte.ramos@gmail.com>
2023-02-26 00:12:40 -0400

    feat: defining outlined variant for Button component

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -5,6 +5,7 @@ import { WithChildren } from "~/interfaces/with-children";
 
 interface Props extends WithChildren, PrimitiveProps {
   label: string;
+  variant?: "default" | "outlined";
 }
 
 type PrimitiveProps = Pick<
@@ -14,16 +15,24 @@ type PrimitiveProps = Pick<
 
 export const Button = ({
   label,
+  variant = "default",
   type = "button",
   onClick,
   disabled = false,
   children,
 }: Props) => {
+  const isDefault = variant === "default";
+  const isOutlined = variant === "outlined";
+
   const className = cx(
     "py-2 px-4 focus:outline-none rounded-md lowercase font-bold",
     {
-      "bg-black text-white": !disabled,
-      "bg-silver text-black cursor-not-allowed": disabled,
+      "bg-black text-white": isDefault && !disabled,
+      "bg-silver text-black": isDefault && disabled,
+      "border-2": isOutlined,
+      "border-black text-black": isOutlined && !disabled,
+      "border-silver text-silver": isOutlined && disabled,
+      "cursor-not-allowed": disabled,
     }
   );
 
